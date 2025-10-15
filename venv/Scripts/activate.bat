956141ee91febbfb9ce422dbc4081e0184078ebc
Pedro221108 <pedroartb0d4@gmail.com>
2025-10-15 15:42:19 -0300

    ChatBot2

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\LunaeGroup\GitProjects\whatsapp-python-chatbot\venv"
+set "VIRTUAL_ENV=C:\Users\Pedro Henrique\Documents\GitHub\whatsapp-python-chatbot\venv"
 
 if not defined PROMPT set PROMPT=$P$G
 
